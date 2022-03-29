46866b735808b2df970f0f4c4c89e65a0a95ca34
Daredo-Python-Lua <68143025+ryoojiz@users.noreply.github.com>
2022-03-29 18:39:14 +0700

    o em ghee

---
+++ wp-content/plugins/nicepage/includes/controls/search/search.php
@@ -0,0 +1,9 @@
+<?php
+defined('ABSPATH') or die;
+
+if (isset($controlProps) && isset($controlTemplate)) {
+    $controlTemplate = str_replace('[[action]]', home_url('/'), $controlTemplate);
+    $controlTemplate = str_replace('[[query]]', esc_html(get_search_query()), $controlTemplate);
+    $controlTemplate = str_replace('[[placeholder]]', __('Search', 'default'), $controlTemplate);
+    echo $controlTemplate;
+}
